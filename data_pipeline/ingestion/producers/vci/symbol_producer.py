a8de07e1d10659be99163750ee8f6628e6d76182
Ryan Vien <dung.vien.sts@tamara.co>
2025-04-22 04:16:00 +0700

    adding ohlc to kafka

--- data_pipeline/ingestion/producers/vci/symbol_producer.py
+++ data_pipeline/ingestion/producers/vci/symbol_producer.py
@@ -4,6 +4,7 @@ import hashlib
 from confluent_kafka import Producer 
 from dotenv import load_dotenv
 from data_pipeline.ingestion.producers.vci.common import get_all_symbols
+from data_pipeline.ingestion.utils.kafka import produce_to_kafka
 import redis
 load_dotenv()
 
@@ -15,23 +16,6 @@ def _get_hash(string: str) -> str:
         str: The hash of the data.
     """
     return hashlib.sha256(string.encode()).hexdigest()
-
-def _produce_to_kafka(topic: str, key: str, value: str) -> None:
-    """
-    Produce data to Kafka.
-    
-    Args:
-        producer (Producer): The Kafka producer instance.
-        topic (str): The Kafka topic to produce to.
-        key (str): The key for the message.
-        value (str): The value for the message.
-    """
-    conf = {
-        'bootstrap.servers': os.getenv("KAFKA_BOOTSTRAP_SERVERS")
-    }
-    producer = Producer(conf)
-    producer.produce(topic, key=key, value=value.encode("utf-8"))
-    producer.flush()
 
 if __name__ == "__main__":
         
@@ -53,14 +37,14 @@ if __name__ == "__main__":
         else:
             print("New symbols found.")
             redis_client.set(symbols_key, new_symbols)
-            _produce_to_kafka(topic="stock.vci.symbols.v1",
+            produce_to_kafka(topic="stock.vci.symbols.v1",
                               key=f"symbols_{str(datetime.datetime.now().timestamp())}",
                               value=new_symbols
                               )
     else:
         print("No cache found. Storing new symbols.")
         redis_client.set(symbols_key, new_symbols)
-        _produce_to_kafka(topic="stock.vci.symbols.v1",
+        produce_to_kafka(topic="stock.vci.symbols.v1",
                             key=f"symbols_{str(datetime.datetime.now().timestamp())}",
                             value=new_symbols
                             )
---
+++ data_pipeline/ingestion/utils/kafka.py
@@ -0,0 +1,26 @@
+import os 
+from confluent_kafka import Producer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def produce_to_kafka(topic: str, key: str, value: str) -> None:
+    """
+    Produce data to Kafka.
+    
+    Args:
+        producer (Producer): The Kafka producer instance.
+        topic (str): The Kafka topic to produce to.
+        key (str): The key for the message.
+        value (str): The value for the message.
+    """
+    conf = {
+        'bootstrap.servers': os.getenv("KAFKA_BOOTSTRAP_SERVERS")
+    }
+    try:
+        producer = Producer(conf)
+        producer.produce(topic, key=key, value=value.encode("utf-8"))
+        producer.flush(30)
+    except Exception as e:
+        print(f"Error producing to Kafka: {e}")
